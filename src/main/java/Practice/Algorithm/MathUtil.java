a5de63c1243971265d392c4f171bfbeb3d6184d0
akhilesh <akhilesh.gupta@adeptia.com>
2020-09-03 22:47:37 +0530

    //Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements. 	// eg Input arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2, n =7 	// solution arr[] = [3, 4, 5, 6, 7, 1, 2]

---
+++ src/main/java/Practice/Algorithm/MathUtil.java
@@ -0,0 +1,11 @@
+package Practice.Algorithm;
+
+public class MathUtil {
+	
+	public static void printArray(int arr[]) {
+		for (int num : arr) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+}
