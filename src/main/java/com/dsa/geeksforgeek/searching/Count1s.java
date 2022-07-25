013ad6e8b898c5ee5e8db4aba497941c7222aebd
akhileshgupta <akhilesh.gupta10011996@gmail.com>
2022-07-25 06:57:34 +0530

    Sliding Window

--- src/main/java/com/dsa/geeksforgeek/searching/Count1s.java
+++ src/main/java/com/dsa/geeksforgeek/searching/Count1s.java
@@ -9,7 +9,7 @@ public class Count1s {
             int mid = (low + high) / 2;
 
             if (arr[mid] == 0)
-                return mid + 1;
+                low= mid + 1;
             else {
                 if (mid == 0 || arr[mid - 1] == 0)
                     return arr.length - mid;
@@ -20,5 +20,10 @@ public class Count1s {
         }
         return 0;
 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0,0,1,1,1,1,1};
+        System.out.println(countOnes(arr));
     }
 }
---
+++ src/main/java/com/dsa/geeksforgeek/string/LeftRepeatCharacter.java
@@ -0,0 +1,20 @@
+package com.dsa.geeksforgeek.string;
+
+public class LeftRepeatCharacter {
+    public static int leftMostCharacter(String str){
+        int characterCount = 256;
+        char[] arr = new char[characterCount];
+        for (int i=0;i<str.length();i++){
+            arr[str.charAt(i)]++;
+        }
+        for(int i=0;i<str.length();i++){
+            if(arr[str.charAt(i)]>1)
+                return i;
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(leftMostCharacter("abccbd"));
+    }
+}
