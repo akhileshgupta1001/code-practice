87e15613f3d3b755ddf652daacc55d77560e8975
akhileshgupta <akhilesh.gupta10011996@gmail.com>
2022-09-20 09:34:00 +0530

    Sliding Window

---
+++ src/main/java/com/dsa/geeksforgeek/arrays/MinimumPlatformRequired.java
@@ -0,0 +1,39 @@
+package com.dsa.geeksforgeek.arrays;
+
+import java.util.Arrays;
+
+public class MinimumPlatformRequired {
+    //https://www.geeksforgeeks.org/minimum-number-platforms-required-railwaybus-station/
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    //https://www.geeksforgeeks.org/minimum-number-platforms-required-railwaybus-station/
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int i=0,j=0;
+        int result = 0;
+        int count =0;
+
+        while(i<n){
+            if(arr[i]<=dep[j]){
+                count++;
+                result = Math.max(count,result);
+                i++;
+            }else{
+                j++;
+                count--;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 100, 300, 500 };
+        int dep[] = { 900, 400, 600 };
+        int n = 3;
+        System.out.println(findPlatform(arr, dep, n));
+    }
+}
